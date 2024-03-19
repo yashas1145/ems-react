b2df0f71b9f53210eb8324ce3d7b38d5849580af
Yashas BN <yashas1145@gmail.com>
2024-03-19 22:53:41 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HelloWorld from "./component/HelloWorld";
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello Vite React!</h1>
+      <HelloWorld />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HelloWorld() {
+    return (
+        <>
+            <h2>Hello Yashas!!!</h2>
+        </>
+    );
+}
+
+export default HelloWorld;
